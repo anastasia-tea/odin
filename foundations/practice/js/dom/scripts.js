dc242575c774416c21f06a0f34ae31bd773012ef
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-20 11:52:20 -0500

    Completed parts 1 and 2

--- foundations/practice/js/dom/scripts.js
+++ foundations/practice/js/dom/scripts.js
@@ -7,3 +7,21 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+//1
+const red = document.createElement("p");
+red.classList.add("red");
+red.textContent = "Hey I'm red!";
+red.style.color = "red";
+
+container.appendChild(red);
+
+//2
+const blue = document.createElement("h3");
+blue.classList.add("blue");
+blue.textContent = "I'm a blue h3!";
+
+container.appendChild(blue);
+
+//3
+const pink = document.createElement("div");
