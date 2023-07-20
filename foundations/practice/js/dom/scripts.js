c71a244d82c422292cec7e74f97d98289239e7a6
anastasia-tea <anastasia.chapinfortin@gmail.com>
2023-07-20 12:02:55 -0500

    Completed part 3!

--- foundations/practice/js/dom/scripts.js
+++ foundations/practice/js/dom/scripts.js
@@ -20,8 +20,25 @@ container.appendChild(red);
 const blue = document.createElement("h3");
 blue.classList.add("blue");
 blue.textContent = "I'm a blue h3!";
+blue.style.color = "blue";
 
 container.appendChild(blue);
 
 //3
 const pink = document.createElement("div");
+pink.classList.add("pink");
+pink.style.border = "solid black";
+pink.style = "background-color: pink;";
+
+const hOne = document.createElement("h1");
+hOne.classList.add("hOne");
+hOne.textContent = "I'm in a div";
+
+const para = document.createElement("p");
+para.classList.add("para");
+para.textContent = "ME TOO!";
+
+pink.appendChild(hOne);
+pink.appendChild(para);
+
+container.appendChild(pink);
